0af81bd369a8753c39e8e6d3b0da6aa979500869
daev <devnull@localhost>
2008-08-02 22:12:28 +0000

    profile admin fix

--- turbion/profiles/admin.py
+++ turbion/profiles/admin.py
@@ -10,7 +10,7 @@ from django.contrib import admin
 from turbion.profiles.models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("username", "first_name", "last_name", "email", "birth_date", "gender")
+    list_display = ("username", "nickname", "email", "site", "is_confirmed", "ip", "birth_date", "gender")
     list_per_page = 25
     list_filter = ("gender", "country", "city")
 
