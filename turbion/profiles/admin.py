8ff97c11619689d7accd1f16242462a2d5fb70e6
daev <devnull@localhost>
2008-08-02 22:14:22 +0000

    profile admin fix

--- turbion/profiles/admin.py
+++ turbion/profiles/admin.py
@@ -12,6 +12,6 @@ from turbion.profiles.models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ("username", "nickname", "email", "site", "is_confirmed", "ip", "birth_date", "gender")
     list_per_page = 25
-    list_filter = ("is_confirmed", "is_superuser", "is_stuff", "country", "city", "gender")
+    list_filter = ("is_confirmed", "is_superuser", "is_staff", "country", "city", "gender")
 
 admin.site.register(Profile, ProfileAdmin)
