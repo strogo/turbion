b64ea4535251d890f25792aa561e454ac5851bbc
daev <devnull@localhost>
2008-08-08 09:57:20 +0000

    configure() added

--- turbion/__init__.py
+++ turbion/__init__.py
@@ -1,18 +1,48 @@
 # -*- coding: utf-8 -*-
-from django.conf import global_settings
 
-from turbion import settings
+def prepare_turbion_settings(outer_settings, options):
+    from django.conf import global_settings
+    from django.core.exceptions import ImproperlyConfigured
+    
+    from turbion import settings
 
-def configure(**options):
-    import inspect
-
-    settings_scope = inspect.stack()[1][0].f_locals
-
+    result = {}
+    
+    options = dict([(key.upper(), value) for key, value in options.items()])
+    
     for name in dir(settings):
         if name.upper() == name:
             if "TURBION_" in name:
                 raw_name = name[8:]
+                
+                result[name] = options.pop(raw_name, getattr(settings, name))           
             else:
-                raw_name = name
-
-            
+                base_value = None
+                if name in outer_settings:
+                    base_value = outer_settings[name]
+                else:
+                    base_value = getattr(global_settings, name, None)
+                    
+                value = getattr(settings, name)
+                
+                if callable(value):
+                    if base_value:
+                        value = value(base_value)
+                    else:
+                        value = value()
+                elif base_value:
+                    raise ImproperlyConfigured("Settings value conflict for %s" % name)
+                
+                result[name] = value
+    
+    if options:
+        result.update([("TURBION_%s" % key, value) for key, value in options.items()])
+    
+    return result
+                
+def configure(**options):
+    import inspect
+    
+    outer_scope = inspect.stack()[1][0].f_locals
+       
+    outer_scope.update(prepare_turbion_settings(outer_scope, options))
