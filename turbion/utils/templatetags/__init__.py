e376f02924e95ab4a2cc93c397c474c92b5a72ca
daev <devnull@localhost>
2008-08-15 14:32:28 +0000

    utils.templatetags improvements

---
+++ turbion/utils/templatetags/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from inspect import getargspec
+
+from django.template import TemplateSyntaxError, Node, Variable, generic_tag_compiler
+from django.utils.functional import curry
+
+def simple_tag_with_request( register ):
+    def _wrapper( func ):
+        decorated_function = getattr(func, "_decorated_function", func)
+        func_name = decorated_function.__name__
+
+        params, xx, xxx, defaults = getargspec(func)
+        if params[0] == 'request':
+            params = params[1:]
+        else:
+            raise TemplateSyntaxError("Any tag function must have a first argument of 'request'")
+
+        class RequestNode(Node):
+            def __init__(self, vars_to_resolve):
+                self.vars_to_resolve = map(Variable, vars_to_resolve)
+
+            def render(self, context):
+                resolved_vars = [var.resolve(context) for var in self.vars_to_resolve]
+                args = [context["request"]] + resolved_vars
+                value = func(*args)
+                return value
+
+        compile_func = curry(generic_tag_compiler, params, defaults, func_name, RequestNode)
+        compile_func.__doc__ = func.__doc__
+        register.tag( func_name, compile_func)
+        return func
+    return _wrapper
