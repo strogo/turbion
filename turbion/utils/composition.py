eb736da43c90745a044bc3287659b861ba1cd1e1
daev <devnull@localhost>
2008-09-25 12:00:27 +0000

    composition field works

--- turbion/utils/composition.py
+++ turbion/utils/composition.py
@@ -1,6 +1,49 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.utils.functional import curry
+from django.utils.itercompat import is_iterable
+
+class Trigger(object):
+    def __init__(self, do, on, field_name, sender, sender_model,\
+                 commit, field_holder_getter, on_update, on_update_initial):
+        self.field_name = field_name
+        self.commit = commit
+        
+        if sender_model and not sender:
+            if isinstance(sender_model, basestring):
+                
+                sender_model = models.get_model(*sender_model.split(".", 1))
+            self.sender = self.sender_model = sender_model
+        else:
+            self.sender = sender
+            self.sender_model = sender_model
+        
+        if not do:
+            raise ValueError("`do` action not defined for trigger")
+        self.do = do
+        
+        if not isinstance(on, (tuple, list)):
+            on = [on]
+        self.on = on
+        
+        self.field_holder_getter = field_holder_getter
+        self.on_update= on_update
+        self.on_update_initial = on_update_initial
+        
+    def connect(self):
+        for signal in self.on:
+            signal.connect(self.handler, sender=self.sender)
+        
+    def handler(self, signal, instance=None, **kwargs):
+        objects = self.field_holder_getter(instance)
+        if not is_iterable(objects):
+            objects = [objects]
+            
+        for obj in objects:
+            setattr(obj, self.field_name, self.do(obj, instance, signal))
+        
+            if self.commit:
+                obj.save()
 
 class CompositionMeta(object):
     def __init__(self, model, field, name, trigger, commons,\
@@ -29,53 +72,51 @@ class CompositionMeta(object):
 
         trigger_defaults = dict(
                 sender_model=model,
+                sender=None,
                 on=[models.signals.post_save],
                 field_holder_getter=lambda instance: instance,
-                on_update=False,
-                on_update_initial=None
+                on_update=False,#Fixme: make allow only one on_update method
+                on_update_initial=None,
+                field_name=name,
+                commit=commit
         )
         trigger_defaults.update(commons)
+        
+        on_update = False
+    
+        for count, t in enumerate(trigger):
+            trigger_meta = trigger_defaults.copy()
+            trigger_meta.update(t)
+            
+            if trigger_meta["on_update"]:
+                if not on_update:
+                    on_update = True
+                else:
+                    raise ValueError("Only one trigger may be marked as `on_update`")
+            else:
+                if count == len(trigger) - 1 and not on_update:#last trigger
+                    trigger_meta["on_update"] = True
 
-        for t in trigger:
-            full_trigger = trigger_defaults.copy()
-            full_trigger.update(t)
-
-            if "sender_model" in full_trigger and "sender" not in full_trigger:
-                sender_model = full_trigger["sender_model"]
-                if isinstance(sender_model, basestring):
-                    sender_model = None
-                full_trigger["sender"] = sender_model
-
-            if "do" not in full_trigger:
-                raise ValueError("`do` action not defined for trigger")
-
-            if not isinstance(full_trigger["on"], (tuple, list)):
-                full_trigger["on"] = [full_trigger["on"]]
-
-            for signal in full_trigger["on"]:
-                def _handler(instance, **kwargs):
-                    obj = full_trigger["field_holder_getter"](instance)
-
-                    setattr(obj, self.name, full_trigger["do"](obj, instance, signal))
-
-                signal.connect(_handler, sender=full_trigger["sender_model"])
-
-            self.trigger.append(full_trigger)
+            trigger_obj = Trigger(**trigger_meta)
+            trigger_obj.connect()
+            
+            self.trigger.append(trigger_obj)
 
     def _update_method(self, instance):
         if self.update_method_queryset:
             qs_getter = self.update_method_queryset
         else:
             qs_getter = [instance]
-
         for trigger in self.trigger:
-            if trigger["on_update"]:
-                setattr(instance, self.name, trigger["on_update_initial"])
+            if trigger.on_update:
+                setattr(instance, trigger.field_name, trigger.on_update_initial)
                 if callable(qs_getter):
                     qs = qs_getter(instance)
-
+                else:
+                    qs = qs_getter
+        
                 for obj in qs:
-                    trigger["do"](instance, obj, trigger["on"])
+                    setattr(instance, trigger.field_name, trigger.do(instance, obj, trigger.on))
 
         if self.commit:
             instance.save()
--- turbion/utils/tests/composition.py
+++ turbion/utils/tests/composition.py
@@ -7,6 +7,12 @@ from turbion.utils.composition import CompositionField
 
 D = dict
 
+class Visit(models.Model):
+    event = models.ForeignKey("Event")
+
+    class Meta:
+        app_label="utils"
+
 class Event(models.Model):
     visit_count=CompositionField(
                     native=models.IntegerField(default=0),
@@ -26,17 +32,10 @@ class Event(models.Model):
                         sender_model="utils.Visit",
                         field_holder_getter=lambda visit: visit.event,
                     ),
-                    commit=True, # save field host after appling of doers
+                    commit=True, # save field holder after appling of doers
                     update_method_queryset=lambda event: event.visit_set.all(),
                     update_method_name="sync_visit_count"
                 )
-
-    class Meta:
-        app_label="utils"
-
-class Visit(models.Model):
-    date = models.DateField()
-    event = models.ForeignKey(Event)
 
     class Meta:
         app_label="utils"
@@ -54,16 +53,15 @@ class Movie(models.Model):
     headline=CompositionField(
                     native=models.CharField(max_length=250),
                     trigger=D(
-                                on=signals.pre_save,
-                                do=lambda movie, signal: "%s, by %s" % (movie.title, movie.director.name)
-                        )
+                                sender_model=Person,
+                                field_holder_getter=lambda visit: visit.movie_set.all(),
+                                do=lambda movie, _, signal: "%s, by %s" % (movie.title, movie.director.name)
+                        ),
+                    commit=True
             )
 
     class Meta:
         app_label="utils"
-
-    def update_headline(self):
-        pass
 
 class CompositionFieldTest(TestCase):
     def test_model_field_meta_exists(self):
@@ -71,6 +69,39 @@ class CompositionFieldTest(TestCase):
 
     def test_model_update_method_exists(self):
         self.assert_(hasattr(Event, "sync_visit_count"), "Update method does not exist")
+        
+    def test_event(self):
+        event = Event.objects.create()
+        
+        for i in range(5):
+            Visit.objects.create(event=event)
+        
+        event = Event.objects.get(pk=event._get_pk_val())
+        self.assertEqual(event.visit_count, 5)
+        
+        event.visit_count = 0
+        event.save()
+        
+        event.sync_visit_count()
+        
+        event = Event.objects.get(pk=event._get_pk_val())
+        self.assertEqual(event.visit_count, 5)
+        
+    def test_movie(self):
+        person = Person.objects.create(name="George Lucas")
+        
+        movie = Movie(title="Star Wars Episode IV: A New Hope", director=person)
+        movie.save()
+        movie.update_headline()
+        
+        movie = Movie.objects.get(pk=movie._get_pk_val())
+        self.assertEqual(movie.headline, "Star Wars Episode IV: A New Hope, by George Lucas")
+        
+        person.name = "George W. Lucas"
+        person.save()
+        
+        movie = Movie.objects.get(pk=movie._get_pk_val())
+        self.assertEqual(movie.headline, "Star Wars Episode IV: A New Hope, by George W. Lucas")
 
     def test_model_update_method(self):
         e = Event()
