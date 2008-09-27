011ee9d9717e27990d93ef8c3bac9c489fc499bd
daev <devnull@localhost>
2008-09-27 11:11:37 +0000

    fixed comment admin list display

--- turbion/comments/admin.py
+++ turbion/comments/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from turbion.comments.models import Comment
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('created_on', "created_by", "status", "connection", "postprocesor" )
+    list_display = ('created_on', "created_by", "status", "connection_ct", "connection_id", "postprocesor" )
     list_per_page = 25
 
     date_hierarchy = 'created_on'
