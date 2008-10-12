58768a20344acbb98821cbda6ab4683ce375c4af
daev <devnull@localhost>
2008-10-12 10:09:37 +0000

    openid urls fixes

--- turbion/openid/urls.py
+++ turbion/openid/urls.py
@@ -4,11 +4,11 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('turbion.openid.views',
     url(r'^login/$',                 'authorization.login',        name="openid_login"),
     url(r'^authenticate/$',          'authorization.authenticate', name="openid_authenticate"),
-    url(r'^collect/$',               'authorization.collect',      name="openid_collect" ),
+    url(r'^collect/$',               'authorization.collect',      name="openid_collect"),
 
-    url(r'^server/endpoint/$',       'server.endpoint',    name="openid_endpoint" ),
-    url(r'^server/decide/$',         'server.decide',      name="openid_decide" ),
-    url(r'^server/xrds/$',           'server.xrds',        name="openid_xrds" ),
+    url(r'^server/endpoint/$',       'server.endpoint',    name="openid_endpoint"),
+    url(r'^server/decide/$',         'server.decide',      name="openid_decide"),
+    url(r'^server/xrds/$',           'server.xrds',        name="openid_xrds"),
 
     (r'^list/$',                     'management.list'),
     (r'^add/$',                      'management.add'),
