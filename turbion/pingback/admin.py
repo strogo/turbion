e2330d9a7ea591a62ace16188c5d4db24309950e
daev <devnull@localhost>
2008-11-03 19:03:15 +0000

    pingback admin fix

--- turbion/pingback/admin.py
+++ turbion/pingback/admin.py
@@ -4,14 +4,14 @@ from turbion.pingback.models import Incoming, Outgoing
 
 class IncomingAdmin(admin.ModelAdmin):
     list_display= ("target_url", "source_url", "date", "title", "status")
-    list_filter = ("content_type",)
+    list_filter = ("descriptor",)
     list_per_page = 25
 
 admin.site.register(Incoming, IncomingAdmin)
 
 class OutgoingAdmin(admin.ModelAdmin):
     list_display = ("target_uri", "date", "title", "status", "rpcserver",)
-    list_filter = ("content_type", )
+    list_filter = ("descriptor", )
     list_per_page = 25
 
 admin.site.register(Outgoing, OutgoingAdmin)
