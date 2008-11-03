26dea9ed6f13886820a256f7febe7e5c210b2221
daev <devnull@localhost>
2008-11-03 19:09:50 +0000

    aliases admin fix

--- turbion/aliases/admin.py
+++ turbion/aliases/admin.py
@@ -3,6 +3,9 @@ from turbion import admin
 from turbion.aliases.models import Alias
 
 class AliasAdmin(admin.ModelAdmin):
-    list_display = ("from_url", "to_url")
+    list_display = (
+        "from_url", "to_url", 'is_active', 'status_code', 'exclude_user_agent'
+    )
+    list_filter = ('is_active', 'status_code', 'exclude_user_agent')
 
 admin.site.register(Alias, AliasAdmin)
