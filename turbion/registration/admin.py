7ba3611c27eeeb25d5b5d2a4c53f5f090b21bf48
daev <devnull@localhost>
2008-11-03 21:51:53 +0000

    fixed registration admin and models

--- turbion/registration/admin.py
+++ turbion/registration/admin.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 from turbion import admin
-from turbion.registration.models import ForbiddenName, Offer
-
-class ForbiddenNameAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(ForbiddenName, ForbiddenNameAdmin)
+from turbion.registration.models import Offer
 
 class OfferAdmin(admin.ModelAdmin):
     list_display = ("user", "type", "date", "code", "data")
-    list_filter = ("code",)
+    list_filter = ("type",)
 
 admin.site.register(Offer, OfferAdmin)
--- turbion/registration/models.py
+++ turbion/registration/models.py
@@ -6,17 +6,6 @@ from turbion.utils.enum import Enum
 
 from turbion.profiles.models import Profile
 
-class ForbiddenName(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = "запретное имя"
-        verbose_name_plural = "запретные имена"
-        db_table = "turbion_reg_forbidden_name"
-
 class Offer(models.Model):
     code_types = Enum(registration    ="registration",
                       email_change    ="email_change",
@@ -31,7 +20,12 @@ class Offer(models.Model):
 
     def gen_code(self):
         import md5
-        return md5.new(self.user.username + self.user.email).hexdigest()
+        import random
+        return md5.new(
+                    self.user.username\
+                    + self.user.email\
+                    + str(random.random())
+                ).hexdigest()
 
     def save(self, *args, **kwargs):
         if not self.code:
