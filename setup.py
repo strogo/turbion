1829a91b03416db59de92d4c602c1461d08cf48a
daev <devnull@localhost>
2008-11-14 21:44:43 +0000

    added setup.py, LICENSE and AUTHOR files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Turbion",
+    version = "0.6",
+    packages = find_packages(),
+
+    install_requires = [
+        'Imaging>=1.1.6',
+        'Pytils',
+    ],
+
+    author = "Alex Koshelev",
+    author_email = "daevaorn@gmail.com",
+    description = "Flexible django-base blog application",
+    license = "New BSD",
+    url = "http://turbion.org/",
+)
