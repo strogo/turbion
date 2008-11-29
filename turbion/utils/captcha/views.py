b1ae3e5233c7c98529a7feb00b587648875b79d1
daev <devnull@localhost>
2008-11-29 13:18:43 +0000

    removed debug print

--- turbion/utils/captcha/views.py
+++ turbion/utils/captcha/views.py
@@ -3,7 +3,6 @@ from turbion.utils.captcha import CaptchaManager
 from django.http import HttpResponse
 
 def image(request, id):
-    print request.session.items()
     response = HttpResponse(mimetype ='image/jpeg')
     CaptchaManager(request.session).render_test(response, str(id))
 
