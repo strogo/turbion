85ad15aa07655549a9fc593185d5b46b071a32a9
daev <devnull@localhost>
2009-01-31 23:18:15 +0000

    updated setup script

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ use_setuptools()
 
 from setuptools import setup, find_packages
 
+# assume that it is relative import
+from turbion import get_version
+
 setup(
     name = "Turbion",
-    version = "0.6",
+    version = get_version(),
     packages = find_packages(),
 
     scripts = ['turbion/bin/turbion-admin.py'],
