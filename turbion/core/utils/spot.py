46b555497b3a39b3c649c447483aa90d3a69d81a
daev <devnull@localhost>
2009-02-11 17:56:25 +0000

    added spot mini-framework

---
+++ turbion/core/utils/spot.py
@@ -0,0 +1,43 @@
+
+class Spot(object):
+    pass
+
+class SpotManager(object):
+    def __init__(self):
+        self._objects = []
+
+    def all(self):
+        return self._objects
+
+    def create(self, obj_type):
+        return obj_type()
+
+    def add(self, obj_type):
+        self._objects.append(self.create(obj_type))
+
+class SpotMeta(object):
+    def __init__(self, name, descriptor):
+        self.name = name
+        self.descriptor = descriptor
+
+def create(base=Spot, manager=SpotManager, meta=SpotMeta):
+    class _SpotMetaclass(type):
+        def __new__(cls, name, bases, attrs):
+            try:
+                _Spot
+            except NameError:
+                cls.manager = manager()
+                t = super(_SpotMetaclass, cls).__new__(cls, name, bases, attrs)
+            else:
+                t = super(_SpotMetaclass, cls).__new__(cls, name, bases, attrs)
+
+                t.meta = meta(name, "%s.%s" % (t.__module__, name))
+
+                cls.manager.add(t)
+
+            return t
+
+    class _Spot(base):
+        __metaclass__ = _SpotMetaclass
+
+    return _Spot
