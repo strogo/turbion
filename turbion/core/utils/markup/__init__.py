48fcecbca896fb73b7e0746b53ce2e9a6089befd
daev <devnull@localhost>
2009-02-11 20:05:40 +0000

    fixed markup imports

--- turbion/core/utils/markup/__init__.py
+++ turbion/core/utils/markup/__init__.py
@@ -1,3 +1,1 @@
-# -*- coding: utf-8 -*-
-from turbion.core.utils.markup.backends.base import BaseProcessor, ProcessorSpot,\
-                                                        postprocess
+
--- turbion/core/utils/markup/filters/__init__.py
+++ turbion/core/utils/markup/filters/__init__.py
@@ -4,14 +4,14 @@ class FilterManager(spot.DictSpotManager):
     def load(self):
         from turbion.core.utils.loading import get_sub_modules
         get_sub_modules("turbion.core.utils.markup", "filters")
-    
+
     def get(self, key):
         self.load()
         return super(FIlterManager, self).get(key)
 
     def all(self):
         self.load()
-        return super(FilterManager).all()
+        return super(FilterManager, self).all()
 
     def add(self, obj_type):
         obj = self.create(obj_type)
