387efd1f53e30a8c69b60975d53f31fec39f902f
Alex Koshelev <daevaorn@gmail.com>
2009-03-28 18:47:35 +0300

    added query tag for smart pagination

--- turbion/core/utils/templatetags/pagination.py
+++ turbion/core/utils/templatetags/pagination.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
 from django.conf import settings
 from django import template
 from django.template.loader import render_to_string
+from django.utils.http import urlencode
+from django.utils.encoding import smart_str
 
 from turbion.core.utils.pagination import split_domains
 
@@ -22,3 +23,38 @@ def paginator(page, template="paginator.html", head_num=2, tail_num=1, left_num=
         'tail': tail
     }
     return  render_to_string(template, context)
+
+class QueryNode(template.Node):
+    def __init__(self, kwargs):
+        self.kwargs = kwargs
+
+    def render(self, context):
+        request = context['request']
+
+        data = request.GET.copy()
+        data.update(
+            dict([(smart_str(k,'ascii'), v.resolve(context))
+                       for k, v in self.kwargs.items()])
+        )
+
+        if data:
+            return urlencode(dict([(k, v) for k, v in data.iteritems() if v not in (None, '')]))
+        else:
+            return ''
+
+@register.tag
+def query(parser, token):
+    bits = token.contents.split(' ')
+    if len(bits) < 2:
+        raise TemplateSyntaxError("'%s' takes one argument"
+                                  " (list of name=value pairs)" % bits[0])
+
+    kwargs = {}
+
+    for arg in bits[1].split(","):
+        if '=' in arg:
+            k, v = arg.split('=', 1)
+            k = k.strip()
+            kwargs[k] = parser.compile_filter(v)
+
+    return QueryNode(kwargs)
