654c202fec2fc8e18beaf6338bf8eba518e822c0
Alex Koshelev <daevaorn@gmail.com>
2009-04-18 20:22:03 +0400

    added repository version fetching for version string formating

--- turbion/__init__.py
+++ turbion/__init__.py
@@ -1,8 +1,44 @@
 import logging
+import os
+import operator
 
-VERSION = (0, 8, 2, 'Pushkin', 0)
+VERSION = (0, 8, 2, 'Pushkin', '0')
 
-def get_version():
-    return ".".join(map(str, VERSION[:3])) + " " + VERSION[3]
+def get_revision(path=None, check_changes=False):
+    if not path:
+        path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    try:
+        from mercurial import hg, ui, repo
+
+        try:
+            repository = hg.repository(ui.ui(), path)
+        except repo.RepoError:
+            return None
+
+        tip = repository.changelog.tip()
+        tip_tag = repository.tagslist()[-1][0]
+
+        if check_changes and bool(reduce(operator.add, repository.status()[:4])):
+            raise RuntimeError('Repository has local modifications')
+
+        return (
+            repository.changelog.nodemap[tip],
+            hg.short(tip),
+            tip_tag
+        )
+    except ImportError:
+        return None
+
+def get_version(bits=4):
+    base = reduce(
+        lambda l, r: (isinstance(r, basestring) and ' ' or '.').join(map(str, [l, r])),
+        VERSION[:bits]
+    )
+    rev = get_revision()
+    if rev:
+        base += " HG-%s:%s:%s" % rev
+
+    return base
 
 logger = logging.getLogger('turbion')
