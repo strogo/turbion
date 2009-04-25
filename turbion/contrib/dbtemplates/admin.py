198a36bbe84d9b8b5e4d6e7a8438d2fd4832b1ab
Alex Koshelev <daevaorn@gmail.com>
2009-04-26 00:54:06 +0400

    removed contrib dbtemplates

--- turbion/contrib/dbtemplates/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from turbion.contrib.dbtemplates.models import Template
-
-class TemplateAdmin(admin.ModelAdmin):
-    list_display = ("path", "is_active",)
-    list_filter = ("is_active",)
-
-admin.site.register(Template, TemplateAdmin)
