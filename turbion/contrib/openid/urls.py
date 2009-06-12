4f09070db7da2b672517e59fc8419f1239467079
Alex Koshelev <daevaorn@gmail.com>
2009-06-12 21:08:52 +0400

    renamed authorization views module to auth

--- turbion/contrib/openid/urls.py
+++ turbion/contrib/openid/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('turbion.contrib.openid.views',
-    url(r'^xrds/$',                  'xrds',                       name="turbion_openid_xrds"),
+    url(r'^xrds/$',                  'xrds',               name="turbion_openid_xrds"),
 
-    url(r'^login/$',                 'authorization.login',        name="turbion_openid_login"),
-    url(r'^authenticate/$',          'authorization.authenticate', name="turbion_openid_authenticate"),
+    url(r'^login/$',                 'auth.login',         name="turbion_openid_login"),
+    url(r'^authenticate/$',          'auth.authenticate',  name="turbion_openid_authenticate"),
 
     url(r'^server/endpoint/$',       'server.endpoint',    name="turbion_openid_endpoint"),
     url(r'^server/decide/$',         'server.decide',      name="turbion_openid_decide"),
