cfe96bab0a5a0c28ec64f0dfcc13645b96093200
daevaorn <devnull@localhost>
2009-10-25 02:27:59 +0400

    Removed server submodule from pingback

--- turbion/bits/pingback/views.py
+++ turbion/bits/pingback/views.py
@@ -1,16 +1,71 @@
-from django import http
-from django.shortcuts import *
-from django.conf import settings
+import urllib2
+from urlparse import urlparse, urlsplit
 
+from django import http
+from django.contrib.sites.models import Site
+
+from turbion.bits.utils.urlfetch import fetch
+from turbion.bits.pingback import signals, utils, client
+from turbion.bits.pingback.models import Pingback
+from turbion.models import Post
 from turbion.bits.utils import xmlrpc
-from turbion.bits.pingback import utils, server, signals, models
 
 gateway = xmlrpc.ServerGateway("pingback")
 
 @gateway.connect
-def ping(source_uri, target_uri, model_id, id):
+def ping(source_uri, target_uri, id):
+    pingback, created = Pingback.objects.get_or_create(
+        source_url=source_uri,
+        target_url=target_uri,
+        incoming=True
+    )
+    if not created:
+        raise utils.PingError(0x0030)
+
     try:
-        res = server.ping(source_uri, target_uri, model_id, id)
-        return res["status"]
+        domain = Site.objects.get_current().domain
+        scheme, server, path, query, fragment = urlsplit(target_uri)
+
+        try:
+            post = Post.objects.get(pk=id)
+        except Post.DoesNotExist:
+            raise utils.PingError(0x0021)
+
+        if post.get_absolute_url() != path + query:
+            raise utils.PingError(0x0021)
+
+        pingback.post = post
+
+        try:
+            doc = fetch(source_uri).content
+        except (urllib2.HTTPError, urllib2.URLError), e:
+            raise utils.PingError(0x0010)
+
+        parser = utils.SourceParser(doc)
+        title = pingback.title = parser.get_title()
+        paragraph = pingback.paragraph = parser.get_paragraph(target_uri)
+
+        status = pingback.status = 'Pingback from %s to %s registered. Keep the web talking! :-)' % (source_uri, target_uri)
+
+        pingback.save()
+
+        signals.pingback_recieved.send(
+            sender=post.__class__,
+            instance=post,
+            pingback=pingback
+        )
+
+        response = {
+            "status": status,
+            "source_title": title,
+            "source_paragraph": paragraph,
+            "source_uri": source_uri,
+            "target_uri": target_uri,
+            "model": model
+        }
+        return response['status']
     except utils.PingError, e:
+        pingback.status = e.code
+        pingback.save()
+
         return e.code
